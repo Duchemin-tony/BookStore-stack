71dd4d9809fa90113811528dea7dae36126f7499
Tony <tony@MacBook-Pro-de-Tony.local>
2023-09-01 16:30:36 +0200

    Save a new book with Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,36 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+// Route for Save a new book
+app.post("/books", async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishYear) {
+            return res.status(400).send({ error: "Data not formatted properly" });
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return res.status(201).send({ book });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: "Server error" });
+    }
 });
 
 mongoose
